afef6441abe5ec7d75e1f41f87880f8826590a4a
Hardik Patel <hardikmystyle1988@gmail.com>
2017-09-16 15:34:21 +0530

    Updating file for deploying spring boot jar on wildfly server
    Updating file for deploying spring boot jar on wildfly server

--- src/main/java/com/blog/hardikpat/controller/HardikPatApplication.java
+++ src/main/java/com/blog/hardikpat/controller/HardikPatApplication.java
@@ -11,7 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication(scanBasePackages="com.blog.hardikpat")
 @EntityScan(basePackages="com.blog.hardikpat")
 @EnableJpaRepositories(basePackages="com.blog.hardikpat")
-public class HardikPatApplication {
+public class HardikPatApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		 SpringApplication.run(HardikPatApplication.class); 
